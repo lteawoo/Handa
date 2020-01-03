795dd077b33b7f11ecd7297eb5055d76d6f318ba
Taewoo Lee <lteawoo@naver.com>
2020-01-03 20:10:09 +0900

    필요없는 import 삭제

--- src/test/java/kr/taeu/handa/todoItem/domain/TestTodoItemRepository.java
+++ src/test/java/kr/taeu/handa/todoItem/domain/TestTodoItemRepository.java
@@ -1,7 +1,6 @@
 package kr.taeu.handa.todoItem.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
 
