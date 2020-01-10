589a6324ca8ccfeaed32b918633dc4b62c43e671
Taewoo Lee <lteawoo@naver.com>
2020-01-10 12:00:14 +0900

    member 도메인 및 필요 모델 작성

--- src/main/java/kr/taeu/handa/member/domain/Member.java
+++ src/main/java/kr/taeu/handa/member/domain/Member.java
@@ -1,5 +1,62 @@
 package kr.taeu.handa.member.domain;
 
-public class Member {
+import java.time.LocalDateTime;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import kr.taeu.handa.global.model.Email;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="MEMBER")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID", updatable = false)
+	private long id;
+	
+	@Embedded
+	@AttributeOverride(name = "value", column = @Column(name = "EMAIL", nullable = false, unique = true, updatable = false, length = 50))
+	private Email email;
+	
+	@Embedded
+	@AttributeOverride(name = "value", column = @Column(name = "UNIQUE_CODE", nullable = false, unique = true, updatable = false, length = 32))
+	private UniqueCode uniqueCode;
+	
+	@Column(name = "NICKNAME", length = 50)
+	private String name;
+	
+	@CreationTimestamp
+	@Column(name = "CREATE_DT", nullable = false, updatable = false)
+	private LocalDateTime createDt;
+	
+	@UpdateTimestamp
+	@Column(name = "UPDATE_DT", nullable = false)
+	private LocalDateTime updateDt;
+	
+	@Builder
+	public Member(Email email, UniqueCode uniqueCode, String name) {
+		this.email = email;
+		this.uniqueCode = uniqueCode;
+		this.name = name;
+	}
+	
+	public void updateProfile(final String name) {
+		this.name = name;
+	}
 }
---
+++ src/main/java/kr/taeu/handa/member/domain/UniqueCode.java
@@ -0,0 +1,31 @@
+package kr.taeu.handa.member.domain;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UniqueCode {
+	
+	@Column(name = "UNIQUE_CODE", length = 50)
+	private String value;
+	
+	private UniqueCode(String value) {
+		this.value = value;
+	}
+	
+	public static UniqueCode build(final String value) {
+		return new UniqueCode(value);
+	}
+	
+	public static UniqueCode generateCode() {
+		return new UniqueCode(UUID.randomUUID().toString().replace("-", ""));
+	}
+}
