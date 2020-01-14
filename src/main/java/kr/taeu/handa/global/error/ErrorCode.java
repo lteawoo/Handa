5a89cd6e01010be0526c0c9b35cc2d094efc333c
Taewoo Lee <lteawoo@naver.com>
2020-01-14 20:00:44 +0900

    알맞는 에러코드로 변경 NotFound -> 404

--- src/main/java/kr/taeu/handa/global/error/ErrorCode.java
+++ src/main/java/kr/taeu/handa/global/error/ErrorCode.java
@@ -8,7 +8,7 @@ public enum ErrorCode {
 	INVALID_TYPE_VALUE(400, "C001", "입력값이 올바르지 않습니다."),
 	
 	// TodoItem
-	ITEM_NOT_FOUND(400, "T001", "아이템을 찾을 수 없습니다.");
+	ITEM_NOT_FOUND(404, "T001", "아이템을 찾을 수 없습니다.");
 	
 	private final String code;
 	private final String message;
