9d7eecaeab4b86bcd6a8727e20e6180f63804fec
Taewoo Lee <lteawoo@naver.com>
2020-01-14 19:24:40 +0900

    MemberDto, Repository 추가

---
+++ src/main/java/kr/taeu/handa/member/dao/MemberRepository.java
@@ -0,0 +1,9 @@
+package kr.taeu.handa.member.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kr.taeu.handa.member.domain.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long>{
+
+}
---
+++ src/main/java/kr/taeu/handa/member/dto/MemberDto.java
@@ -0,0 +1,5 @@
+package kr.taeu.handa.member.dto;
+
+public class MemberDto {
+
+}
