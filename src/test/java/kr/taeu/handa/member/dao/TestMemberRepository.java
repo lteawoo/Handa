634b9e4eea9a9a53a0ce5290e776039104006826
Taewoo Lee <lteawoo@naver.com>
2020-03-02 14:15:39 +0900

    MemberDetails Service, Repository 테스트

--- src/test/java/kr/taeu/handa/member/dao/TestMemberRepository.java
+++ src/test/java/kr/taeu/handa/member/dao/TestMemberRepository.java
@@ -9,30 +9,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import kr.taeu.handa.domain.member.dao.MemberDetailsRepository;
+import kr.taeu.handa.domain.member.domain.Member;
+import kr.taeu.handa.domain.member.domain.model.Email;
+import kr.taeu.handa.domain.member.domain.model.Name;
+import kr.taeu.handa.domain.member.domain.model.Password;
+import kr.taeu.handa.domain.member.domain.model.Role;
+
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class TestMemberRepository {
 	@Autowired
-	MemberRepository memberRepository;
+	MemberDetailsRepository memberDetailsRepository;
 
 	@AfterEach
 	public void cleanup() {
 		/*
 		 * 이후 테스트 코드에 영향이 없게 테스트 메서드 끝날때 마다 전체 삭제
 		 */
-		memberRepository.deleteAll();
+		memberDetailsRepository.deleteAll();
 	}
 
 	@Test
 	public void 회원가입() {
 		// given
-		Member member = Member.builder().email(Email.build("lteawoo@naver.com")).name("이태우")
-				.password(Password.build("12345")).uniqueCode(UniqueCode.generateCode()).build();
+		Member member = Member.builder()
+				.email(new Email("test@taeu.kr"))
+				.name(new Name("테스트계정"))
+				.password(new Password("12345"))
+				.role(Role.MEMBER)
+				.build();
 
 		// when
-		memberRepository.save(member);
+		memberDetailsRepository.save(member);
 
 		// then
-		assertTrue(memberRepository.existsByEmail(member.getEmail()));
+		assertTrue(memberDetailsRepository.existsByEmail(member.getEmail()));
 	}
 }
--- src/test/java/kr/taeu/handa/member/service/TestMemberService.java
+++ src/test/java/kr/taeu/handa/member/service/TestMemberService.java
@@ -7,14 +7,21 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import kr.taeu.handa.domain.member.dao.MemberDetailsRepository;
+import kr.taeu.handa.domain.member.domain.Member;
+import kr.taeu.handa.domain.member.domain.model.Email;
+import kr.taeu.handa.domain.member.domain.model.Name;
+import kr.taeu.handa.domain.member.domain.model.Password;
+import kr.taeu.handa.domain.member.domain.model.Role;
+import kr.taeu.handa.domain.member.dto.SignUpRequest;
+import kr.taeu.handa.domain.member.exception.EmailAlreadyExistsException;
+import kr.taeu.handa.domain.member.service.MemberDetailsService;
 import kr.taeu.handa.global.error.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,56 +29,64 @@ import lombok.extern.slf4j.Slf4j;
 @ExtendWith(MockitoExtension.class)
 public class TestMemberService {
 	@InjectMocks
-	MemberService memberService;
+	MemberDetailsService memberDetailsService;
 
 	@Mock
-	MemberRepository repo;
+	MemberDetailsRepository memberDetailsRepository;
 
-	@Test
-	public void 없는_회원_조회() {
-		// given
-		given(this.repo.findByEmail(any(Email.class))).willReturn(Optional.empty());
-
-		// when
-		MemberNotFoundException thrown = assertThrows(MemberNotFoundException.class,
-				() -> this.memberService.findByEmail(Email.build("lteawoo@naver.com")));
-		log.info("error : ", thrown);
-
-		// then
-		assertEquals(thrown.getErrorCode(), ErrorCode.MEMBER_NOT_FOUND);
-	}
+//	@Test
+//	public void 없는_회원_조회() {
+//		// given
+//		given(this.memberDetailsRepository.findByEmail(any(Email.class))).willReturn(Optional.empty());
+//
+//		// when
+//		EmailNotFoundException thrown = assertThrows(EmailNotFoundException.class,
+//				() -> this.memberDetailsService.findByEmail(new Email("test@taeu.kr")));
+//		log.info("error : ", thrown);
+//
+//		// then
+//		assertEquals(thrown.getErrorCode(), ErrorCode.MEMBER_NOT_FOUND);
+//	}
 
 	@Test
 	public void 이미_가입한_이메일로_회원가입() {
 		// given
-		SignUpRequest dto = new SignUpRequest(Email.build("lteawoo@naver.com"), "이태우", Password.build("12345"));
-		given(this.repo.existsByEmail(any(Email.class))).willReturn(true);
+		SignUpRequest dto = SignUpRequest.builder()
+				.email(new Email("test@taeu.kr"))
+				.name(new Name("테스트계정"))
+				.password(new Password("12345"))
+				.role(Role.MEMBER)
+				.build();
+		given(this.memberDetailsRepository.existsByEmail(any(Email.class))).willReturn(true);
 
 		// when
-		EmailDuplicateException thrown = assertThrows(EmailDuplicateException.class,
-				() -> this.memberService.signUp(dto));
+		EmailAlreadyExistsException thrown = assertThrows(EmailAlreadyExistsException.class,
+				() -> this.memberDetailsService.createMember(dto));
 		log.info("error : ", thrown);
 
 		// then
-		assertEquals(thrown.getErrorCode(), ErrorCode.EMAIL_DUPLICATION);
+		assertEquals(thrown.getErrorCode(), ErrorCode.REQUEST_CONFILICT_EXCEPTION);
 	}
 
 	@Test
 	public void 가입하지_않은_이메일로_회원가입() {
 		// given
-		Email email = Email.build("lteawoo@naver.com");
-		UniqueCode code = UniqueCode.generateCode();
-		SignUpRequest dto = new SignUpRequest(email, "이태우", Password.build("12345"));
-
-		given(this.repo.existsByEmail(email)).willReturn(false);
-		given(this.repo.save(any(Member.class))).willReturn(dto.toEntity(code));
+		Email email = new Email("test@taeu.kr");
+		SignUpRequest dto = SignUpRequest.builder()
+				.email(email)
+				.name(new Name("테스트 계정"))
+				.password(new Password("12345"))
+				.role(Role.MEMBER)
+				.build();
+		given(this.memberDetailsRepository.existsByEmail(email)).willReturn(false);
+		given(this.memberDetailsRepository.save(any(Member.class))).willReturn(dto.toEntity());
 
 		// when
-		Member signedMember = this.memberService.signUp(dto);
+		Member signedMember = this.memberDetailsService.createMember(dto);
 
 		// then
-		verify(this.repo, atLeastOnce()).existsByEmail(email);
-		verify(this.repo, atLeastOnce()).save(any());
+		verify(this.memberDetailsRepository, atLeastOnce()).existsByEmail(email);
+		verify(this.memberDetailsRepository, atLeastOnce()).save(any());
 		assertEquals(dto.getEmail(), signedMember.getEmail());
 	}
 }
