ba669f2537508fe86d5f403dae4a691be85a9c67
Taewoo Lee <lteawoo@naver.com>
2020-03-03 19:58:37 +0900

    Member 통합테스트

--- src/test/java/kr/taeu/handa/member/TestMember.java
+++ src/test/java/kr/taeu/handa/member/TestMember.java
@@ -1,27 +1,24 @@
-package kr.taeu.handa.member.controller;
+package kr.taeu.handa.member;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import kr.taeu.handa.domain.member.controller.MemberController;
+import kr.taeu.handa.Application;
 import kr.taeu.handa.domain.member.domain.model.Email;
 import kr.taeu.handa.domain.member.domain.model.Name;
 import kr.taeu.handa.domain.member.domain.model.Password;
@@ -30,19 +27,20 @@ import kr.taeu.handa.domain.member.dto.SignUpRequest;
 import kr.taeu.handa.domain.member.service.MemberDetailsService;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = {MemberController.class})
-public class TestMemberController {
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+public class TestMember {
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@MockBean
+	@InjectMocks
 	private MemberDetailsService memberDetailsService;
 	
 	private ObjectMapper objectMapper = new ObjectMapper();
 	
 	@Test
 	@WithAnonymousUser
-	public void 회원가입_요청() throws Exception{
+	public void 통합_테스트_회원가입_요청() throws Exception {
 		//given
 		SignUpRequest signUpRequest = SignUpRequest.builder()
 				.email(new Email("taeu@test.kr"))
@@ -50,7 +48,7 @@ public class TestMemberController {
 				.password(new Password("12345"))
 				.role(Role.MEMBER)
 				.build();
-		given(memberDetailsService.createMember(any(SignUpRequest.class))).willReturn(signUpRequest.toEntity());
+		//given(memberDetailsService.createMember(any(SignUpRequest.class))).willReturn(signUpRequest.toEntity());
 		
 		//when
 		mockMvc.perform(post("/member/signup")
