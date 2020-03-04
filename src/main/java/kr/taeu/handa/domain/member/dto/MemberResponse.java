b87ad319e255783c8bb61bc6d2886d341eb38052
Taewoo Lee <lteawoo@naver.com>
2020-03-04 18:40:11 +0900

    name response 추가

--- src/main/java/kr/taeu/handa/domain/member/dto/MemberResponse.java
+++ src/main/java/kr/taeu/handa/domain/member/dto/MemberResponse.java
@@ -4,16 +4,19 @@ import java.time.LocalDateTime;
 
 import kr.taeu.handa.domain.member.domain.Member;
 import kr.taeu.handa.domain.member.domain.model.Email;
+import kr.taeu.handa.domain.member.domain.model.Name;
 import lombok.Getter;
 
 @Getter
 public class MemberResponse {
 	private Email email;
+	private Name name;
 	private LocalDateTime lastModifiedDate;
 	private LocalDateTime createdDate;
 
 	public MemberResponse(Member member) {
 		this.email = member.getEmail();
+		this.name = member.getName();
 		this.lastModifiedDate = member.getLastModifiedDate();
 		this.createdDate = member.getCreatedDate();
 	}
