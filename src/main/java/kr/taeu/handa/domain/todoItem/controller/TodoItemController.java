33cab8132f5ac01c72ca38043058a7f76b7f7cd6
Taewoo Lee <lteawoo@naver.com>
2020-03-05 21:28:08 +0900

    todoitem 컨트롤러 정비

--- src/main/java/kr/taeu/handa/domain/todoItem/controller/TodoItemController.java
+++ src/main/java/kr/taeu/handa/domain/todoItem/controller/TodoItemController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import kr.taeu.handa.domain.todoItem.domain.TodoItem;
 import kr.taeu.handa.domain.todoItem.dto.TodoItemDto;
+import kr.taeu.handa.domain.todoItem.dto.TodoItemResponse;
+import kr.taeu.handa.domain.todoItem.dto.WriteItemRequest;
 import kr.taeu.handa.domain.todoItem.service.TodoItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,21 +31,21 @@ public class TodoItemController {
 	}
 
 	@PostMapping(value = "/api/item/write")
-	public TodoItemDto.Res write(@RequestBody @Valid final TodoItemDto.WriteReq dto) {
-		return new TodoItemDto.Res(todoItemService.write(dto));
+	public TodoItemResponse write(@RequestBody @Valid final WriteItemRequest dto) {
+		return new TodoItemResponse(todoItemService.write(dto));
 	}
 
-	@PostMapping(value = "/api/item/modifyContent/{id}")
-	public TodoItemDto.Res modifyContent(@PathVariable final Long id,
-			@RequestBody @Valid final TodoItemDto.ModifyContentReq dto) {
-		return new TodoItemDto.Res(todoItemService.modifyContent(id, dto));
-	}
-
-	@PostMapping(value = "/api/item/modifyDone/{id}")
-	public TodoItemDto.Res modifyDone(@PathVariable final Long id,
-			@RequestBody @Valid final TodoItemDto.ModifyDoneReq dto) {
-		return new TodoItemDto.Res(todoItemService.modifyDone(id, dto));
-	}
+//	@PostMapping(value = "/api/item/modifyContent/{id}")
+//	public TodoItemDto.Res modifyContent(@PathVariable final Long id,
+//			@RequestBody @Valid final TodoItemDto.ModifyContentReq dto) {
+//		return new TodoItemDto.Res(todoItemService.modifyContent(id, dto));
+//	}
+//
+//	@PostMapping(value = "/api/item/modifyDone/{id}")
+//	public TodoItemDto.Res modifyDone(@PathVariable final Long id,
+//			@RequestBody @Valid final TodoItemDto.ModifyDoneReq dto) {
+//		return new TodoItemDto.Res(todoItemService.modifyDone(id, dto));
+//	}
 
 	@DeleteMapping(value = "/api/item/delete/{id}")
 	public void delete(@PathVariable final Long id) {
