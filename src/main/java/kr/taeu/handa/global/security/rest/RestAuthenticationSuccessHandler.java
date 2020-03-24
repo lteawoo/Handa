4a9ed948b60c6022baf98f098e317255d9d2d456
Taewoo Lee <lteawoo@naver.com>
2020-03-24 21:24:43 +0900

    cors test..

--- src/main/java/kr/taeu/handa/global/security/rest/RestAuthenticationSuccessHandler.java
+++ src/main/java/kr/taeu/handa/global/security/rest/RestAuthenticationSuccessHandler.java
@@ -1,13 +1,14 @@
 package kr.taeu.handa.global.security.rest;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +22,9 @@ public class RestAuthenticationSuccessHandler implements AuthenticationSuccessHa
 		for(String s : response.getHeaderNames()) {
 			log.info(s + " : " + response.getHeader(s));
 		}
-		PrintWriter writer = response.getWriter();
-		writer.flush();
+//		PrintWriter writer = response.getWriter();
+//		writer.flush();
+		RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+		redirectStrategy.sendRedirect(request, response, "/member/welcome");
 	}
 }
