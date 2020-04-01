fc71f2b2329ea681f09e994d8324e1cd7cc8e963
Taewoo Lee <lteawoo@naver.com>
2020-04-01 17:42:59 +0900

    cors 적용..

--- src/main/java/kr/taeu/handa/global/config/ResourceServerConfig.java
+++ src/main/java/kr/taeu/handa/global/config/ResourceServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableResourceServer
@@ -18,9 +19,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.requestMatchers()
-				.antMatchers("/member/**")
-				.antMatchers("/api/**")
+				.antMatchers("/member/**", "/api/**")
 			.and().authorizeRequests()
+				.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
 				.antMatchers("/member/signup", "/member/signin").anonymous()
 				.antMatchers("/member/test").permitAll()
 				.antMatchers("/member/**").authenticated()
