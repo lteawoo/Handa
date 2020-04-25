6163f30826b28ddc7522e43c191b0349e9baa8d8
Taewoo Lee <lteawoo@naver.com>
2020-04-25 13:56:10 +0900

    import 정리

--- src/main/java/kr/taeu/handa/global/config/SecurityConfig.java
+++ src/main/java/kr/taeu/handa/global/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
